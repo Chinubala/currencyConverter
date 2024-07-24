02a70da4314337bd8259e0c10b70582a9cd2c333
unknown <chinubaladash19@gmail.com>
2024-07-24 20:35:05 +0530

    currency change

--- src/Component/Exchange.js
+++ src/Component/Exchange.js
@@ -13,27 +13,58 @@ import dayjs from 'dayjs';
 const Exchange = () => {
     const [currencyOptions, setCurrencyOptions] = useState([]);
     const [fromCurrency, setFromCurrency] = useState("USD");
-    const [toCurrency, setToCurrency] = useState('');
+    const [toCurrency, setToCurrency] = useState('INR');
     const [exchangeRate, setExchangeRate] = useState('')
-    const [conversionRates , setConversionRates]  = useState([]);
+    const [historyExchangeRate, setHistoryExchangeRate] = useState(0)
+    const [conversionRates, setConversionRates] = useState([]);
     const [amount, setAmount] = useState(1);
-    const [history , setHistory] = useState([])
-    const [dateValue , setDateValue] = useState({year: '', month: '', day: ''})
+    const [history, setHistory] = useState([])
+    const [dateValue, setDateValue] = useState({ year: '', month: '', day: '', fullData: '' })
     const dateFormat = 'YYYY-MM-DD';
+    const todayDate = new Date();
+    const day = todayDate.getDate();
+    const month = todayDate.getMonth() + 1; // getMonth() returns month from 0 to 11
+    const year = todayDate.getFullYear();
+    const dateStr = `${year}-${month}-${day}`;
 
 
     useEffect(() => {
         const BASE_URL = `https://v6.exchangerate-api.com/v6/d88161f4605cd0c43b9f0ec5/latest/${fromCurrency}`
-        console.log("URL", BASE_URL);
 
         fetch(BASE_URL)
             .then(res => res.json())
             .then(data => {
                 const fristCurrency = Object.keys(data.conversion_rates)[0];
                 setCurrencyOptions([data.base_code, ...Object.keys(data.conversion_rates)])
-                setConversionRates([{...data.conversion_rates}])
+                setConversionRates([{ ...data.conversion_rates }])
+            })
+
+        const history_BASE_URL = `https://v6.exchangerate-api.com/v6/d88161f4605cd0c43b9f0ec5/history/${fromCurrency}/${year}/${month}/${day}`;
+        // const history_url = `https://api.fastforex.io/historical?date=${dateString}&api_key=2bf819820f-3f52629cff-sh449q`
+        fetch(history_BASE_URL)
+            .then(res => res.json())
+            .then(data => {
+                setConversionRates([{ ...data?.conversion_rates }])
+                const currencyRate = conversionRates;
+                if (toCurrency !== '') {
+                    if(currencyRate[0]){
+                        setHistoryExchangeRate(currencyRate[0][toCurrency])
+                    }         
+                } else {
+                    setHistoryExchangeRate(0);
+                }
+                console.log("date", dateStr, day , month , year )
+                setDateValue({ year: year, month: month, day: day, fullData: dateStr });
             })
     }, [])
+
+    useEffect(() => {
+        setDateValue({ year: year, month: month, day: day, fullData: dateStr });
+        if(dateValue.year !== ""){
+            historyExchangeRateAPI();
+        }
+      
+    }, [toCurrency]);
     function handleToAmountChange(e) {
         e.preventDefault();
         setAmount(e.target.value)
@@ -49,25 +80,27 @@ const Exchange = () => {
     }
 
     const onChange = async (date, dateString) => {
-        
-        setDateValue({year: date?.$y, month: date?.$M + 1, day: date?.$D})
-        // const history_BASE_URL = `https://v6.exchangerate-api.com/v6/d88161f4605cd0c43b9f0ec5/history/${fromCurrency}/${date?.$y}/${date?.$M + 1}/${date?.$D}`;
-        const history_url = `https://api.fastforex.io/historical?date=${dateString}&api_key=2bf819820f-3f52629cff-sh449q`
-        fetch(history_url)
-            .then(res => res.json())
-            .then(data => { setHistory(data.results);})
-       
-      };
+        const dateStr = `${date?.$y}-${date?.$M + 1}-${date?.$D}`;
+        setDateValue({ year: date?.$y, month: date?.$M + 1, day: date?.$D, fullData: dateStr })
+        historyExchangeRateAPI();
 
-      const getConversionRate = (curr) => {
-          const currencyRate= conversionRates;
-          if(toCurrency !== ''){
-            return currencyRate[0][curr]
-          } else {
-            return 0;
-          }
-          console.log("helloo",currencyRate[0]['AMD'])
-      }
+    };
+
+    const historyExchangeRateAPI = () => {
+        const history_BASE_URL = `https://v6.exchangerate-api.com/v6/d88161f4605cd0c43b9f0ec5/history/${fromCurrency}/${dateValue?.year}/${dateValue?.month}/${dateValue?.day}`;
+        // const history_url = `https://api.fastforex.io/historical?date=${dateString}&api_key=2bf819820f-3f52629cff-sh449q`
+        fetch(history_BASE_URL)
+            .then(res => res.json())
+            .then(data => {
+                setConversionRates([{ ...data.conversion_rates }])
+                const currencyRate = conversionRates;
+                if (toCurrency !== '') {
+                    setHistoryExchangeRate(currencyRate[0][toCurrency])
+                } else {
+                    setHistoryExchangeRate(0);
+                }
+            })
+    }
 
 
     return (
@@ -123,23 +156,27 @@ const Exchange = () => {
                                     </Select>
                                 </FormControl>
                             </div>
+                            <div><button onClick={handleSubmit} className="btn btn1" >Submit</button></div>
+                            <TextField id="outlined-basic" variant="outlined" style={{ width: "72%" }} value={exchangeRate} />
+                            <div className='card-header'>
+                                <h1>Historical Exchange Rates</h1>
+                            </div>
                             <div>
-
+                                <label style={{ paddingLeft: '50px' }}>Change Dates</label>
                                 <Space direction="vertical" style={{ width: "87%" }}>
-                                    <DatePicker 
-                                    onChange={onChange} 
-                                    style={{ width: "87%" }}
-                                    defaultValue={dayjs('2024-07-25', dateFormat)}
-                                    format={dateFormat}
+                                    <DatePicker
+
+                                        onChange={onChange}
+                                        style={{ width: "87%" }}
+                                        defaultValue={dayjs('2024-07-24', dateFormat)}
+                                        format={dateFormat}
                                     />
 
                                 </Space>
                             </div>
-                            <div><button onClick={handleSubmit} className="btn btn1" >Submit</button></div>
-                            <TextField id="outlined-basic" variant="outlined" style={{ width: "72%" }} value={exchangeRate} />
-                          {conversionRates.length > 0 ?  <div>
-                                <h6>{`1 ${fromCurrency} = ${getConversionRate(toCurrency)} ${toCurrency}`}</h6>
-                            </div>: ''}
+                            {historyExchangeRate ? <div>
+                                <h6>{`1 ${fromCurrency} = ${historyExchangeRate} ${toCurrency}`}</h6>
+                            </div> : ''}
                         </div>
                     </div>
                 </form>
